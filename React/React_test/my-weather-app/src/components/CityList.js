a15d9114b69342c3399cfb8581cb891d503e9774
HeeSeung Shin <tlsgmltmd726529@gmail.com>
2021-05-14 00:38:06 +0900

    React

---
+++ React/React_test/my-weather-app/src/components/CityList.js
@@ -0,0 +1,16 @@
+function CityList(props){
+    const {cities} = props;
+
+    return (
+        <div>
+            <h1>CityList</h1>
+            <ul>
+                {cities.map((item, index) => {
+                    return <li key={index}>{item}</li>;
+                })};
+            </ul>
+        </div>
+    )
+}
+
+export  default CityList;
